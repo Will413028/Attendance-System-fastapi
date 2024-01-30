022b6d9e85447ea6d0d8e2fd436fb94004dad82a
will <will413028@gmail.com>
2024-01-30 09:14:09 +0800

    feat: add attendance record table

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Attendance-System-fastapi
+
+
+Run server
+
+```
+uvicorn main:app --reload
+```
+
+Docker build
+```
+sudo docker build -t <image_name>:<tag> .
+```
+
+Docker run
+
+```
+docker run --name test -p 8000:8000 test
+```
+Run test
+```
+python -m pytest
+```
+
+Auto generate migration
+```
+alembic revision --autogenerate -m "migration message"
+```
+
+Do migration
+```
+alembic upgrade head
+```
+
+docker compose
+```
+sudo docker-compose up -d --build 
+```
--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
 
 from database import Base
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     name = mapped_column(String(128), index=True)
     account = mapped_column(String(128), index=True)
@@ -14,3 +14,15 @@ class User(Base):
     role = mapped_column(String(64))
     phone = mapped_column(String(64))
     error_times = mapped_column(Integer, default=0)
+    attendance_records = relationship("AttendanceRecord", back_populates="user")
+
+
+class AttendanceRecord(Base):
+    __tablename__ = 'attendance_records'
+
+    user_id = mapped_column(Integer, ForeignKey('users.id'))
+    attendance_date = mapped_column(DateTime)
+    time_in = mapped_column(DateTime)
+    time_out = mapped_column(DateTime)
+    attendance_type = mapped_column(String(32))
+    user = relationship("User", back_populates="attendance_records")
